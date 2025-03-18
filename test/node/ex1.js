5f907fa5d8c4c2d9dd910dc67683005e199cef6b
kdtTetz <xenosign@naver.com>
2025-03-18 22:03:41 +0900

    250318 수업 준비

---
+++ test/node/ex1.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+  try {
+    const rand = parseInt(Math.random() * 10);
+    console.log(`생성된 수는 ${rand}`);
+    if (rand > 5) return console.log('아싸 로또 사자!');
+    throw new Error('망했어요...');
+  } catch (error) {
+    console.log(error);
+  }
+}, 2000);
