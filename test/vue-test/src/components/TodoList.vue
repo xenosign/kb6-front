4ebb8303bcbed60366b12d2c34ccbd92a9ec5ab3
kdtTetz <xenosign@naver.com>
2025-03-29 21:00:41 +0900

    250329

---
+++ test/vue-test/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<style>
+.todo {
+  text-align: center;
+}
+</style>
+
+<template>
+  <div class="todo">
+    <h1>오늘의 할일</h1>
+    <div class="checklist">
+      <h2>오늘 해야할 일 2개</h2>
+      <h2>오늘 완료한 일 0개</h2>
+    </div>
+    <hr />
+    <h2>Vue 공부하기</h2>
+    <hr />
+    <h2>배열 함수 끝장내기</h2>
+    <hr />
+  </div>
+</template>
